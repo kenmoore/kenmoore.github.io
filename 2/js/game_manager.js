4500d1076dcdddcb9b42ada210493fd393c7159d
Ken Moore <kenmoore@gmail.com>
2014-05-04 11:24:59 -0700

    Add Analytics tracking

--- 2/js/game_manager.js
+++ 2/js/game_manager.js
@@ -68,7 +68,8 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2048 : 1024;
+//    var value = Math.random() < 0.9 ? 2048 : 1024;
+    var value = Math.random() < 0.9 ? 32 : 16;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
